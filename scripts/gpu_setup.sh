ef00d28c55bad92d5f590336f44edb9089fdf177
Lloyd <lloyd.w.henning@gmail.com>
2012-03-10 19:30:38 +0000

    made all scripts executable

--- scripts/gpu_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/cuda/lib64"
-CUDA_ROOT="/usr/local/cuda"
-THEANO_FLAGS="cuda.root=/usr/local/cuda,device=gpu,floatX=float32"
-
