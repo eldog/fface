ff6f57f129bc139ccf6a82ce1ed1366a4d7d20da
Lloyd <lloyd.w.henning@gmail.com>
2012-03-12 17:54:23 +0000

    Now able to run first test. Not that it actually tests anything

--- src/conv-net-0.1/test/cvmaxoperatorplane_test.cpp
+++ src/conv-net-0.1/test/cvmaxoperatorplane_test.cpp
@@ -1,4 +1,16 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE cvmaxoperatorplane test
+
 #include <boost/test/unit_test.hpp>
+#include <opencv/cv.h>
+
+#include "cvmaxoperatorplane.h"
+
+BOOST_AUTO_TEST_CASE( cvmaxoperatorplane_test )
+{
+    CvSize featureMap = cvSize(2, 2);
+    CvSize neuronSize = cvSize(2, 2);
+    CvMaxOperatorPlane maxOperatorPlane("test", featureMap, neuronSize);
+} // BOOST_AUTO_TEST_CASE 
+
 
